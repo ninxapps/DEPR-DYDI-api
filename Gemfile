fb3bbf4102a0c12eb7236a15d857731d5c1ea276
José Castro <n3ggro@gmail.com>
2016-08-05 22:37:43 -0400

    Fake data on seed

--- Gemfile
+++ Gemfile
@@ -24,9 +24,12 @@ gem 'active_model_serializers'
 
 gem 'devise_token_auth'
 
+gem 'faker'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+6.times do |i|
+	Category.create(name: Faker::Lorem.word, nsfw: Faker::Boolean.boolean(0.2))
+end
+
+5.times do |i|
+	name = Faker::Name.first_name
+	User.create(name: name, email: Faker::Internet.email(name), password: '11111111', password_confirmation: '11111111')
+end
+
+50.times do |i|
+	Question.create(text: Faker::Lorem.sentence, category_id: Faker::Number.between(1, 6), user_id: Faker::Number.between(1, 6))
+end
